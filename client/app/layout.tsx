4e30fd3c247e702f8610d8b93fb29efbf47799aa
huzwarsi <huzaifawarsi2006@gmail.com>
2026-08-21 15:24:57 +0500

    style(ui): set default theme back to dark (black mode)

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -107,7 +107,7 @@ import Script from 'next/script';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${manrope.variable} ${jetbrainsMono.variable}`} suppressHydrationWarning>
+    <html lang="en" className={`${manrope.variable} ${jetbrainsMono.variable} dark`} suppressHydrationWarning>
       <head>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-J8FT80QMWD"
